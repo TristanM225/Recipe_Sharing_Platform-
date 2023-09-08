e13a53bef2ecf484665b4af46dae9f5f2a15a727
rayr482 <rar002@ucsd.edu>
2023-09-07 21:29:49 -0700

    added Recipe and Ingredient models

---
+++ server/models/Ingredient.js
@@ -0,0 +1,33 @@
+const { Schema } = require('mongoose');
+
+const ingredientSchema = new Schema({
+    ingredientId: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    aisle: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: Decimal128,
+        required: true,
+    },
+    consistency: {
+        type: String,
+    },
+    unit: {
+        type: String,
+    },
+    meta: [
+        {
+            type: String,
+        },
+    ],
+});
+
+module.exports = ingredientSchema;
--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,29 +1,85 @@
 const { Schema } = require('mongoose');
+const ingredientSchema = require('./Ingredient');
 
 // TODO: figure out what fields we want in the recipe
 const recipeSchema = new Schema({
-  author: [
-    {
-      type: String,
-    },
-  ],
-  description: {
-    type: String,
-    required: true,
-  },
   recipeId: {
-    type: String,
+    type: Number,
     required: true,
-  },
-  image: {
-    type: String,
-  },
-  link: {
-    type: String,
   },
   title: {
     type: String,
     required: true,
+  },
+  ingredients: [ingredientSchema],
+  analyzedInstructions: [
+    {
+      type: String,
+    },
+  ],
+  servings: {
+    type: Number,
+  },
+  readyInMinutes: {
+    type: Number,
+  },
+  instructions: {
+    type: String,
+  },
+  pricePerServing: {
+    type: Decimal128,
+  },
+  cheap: {
+    type: Boolean,
+  },
+  dairyFree: {
+    type: Boolean,
+  },
+  glutenFree: {
+    type: Boolean,
+  },
+  ketogenic: {
+    type: Boolean,
+  },
+  lowFodmap: {
+    type: Boolean,
+  },
+  sustainable: {
+    type: Boolean,
+  },
+  vegan: {
+    type: Boolean,
+  },
+  vegetarian: {
+    type: Boolean,
+  },
+  veryHealthy: {
+    type: Boolean,
+  },
+  veryPopular: {
+    type: Boolean,
+  },
+  whole30: {
+    type: Boolean,
+  },
+  dishTypes: [
+    {
+      type: String,
+    },
+  ],
+  healthScore: {
+    type: Decimal128,
+  },
+  winePairing: [
+    {
+      type: String,
+    },
+  ],
+  image: {
+    type: String,
+  },
+  sourceLink: {
+    type: String,
   },
 });
 

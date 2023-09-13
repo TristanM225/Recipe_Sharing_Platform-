90e773c8d3f521ed218787164c3fcbeaa7293c55
Michael Salisbury <m.slzbry@gmail.com>
2023-09-12 19:24:47 -0700

    fix compilation errors that prevent the app from running

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,7 +1,7 @@
 
 //server/schemas/index.js
 const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const resolvers = require('./resolver');
 
 module.exports = { typeDefs, resolvers }
 
